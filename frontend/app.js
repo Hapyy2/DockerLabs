de371ee4f8eed9605b6894880ddd79a46c132fb2
Mateusz <mateusz.klemann@wp.pl>
2025-04-17 17:52:57 +0200

    quick fixes

--- frontend/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'page')));
-
-app.listen(80, () => {
-  console.log('Frontend is running on port 80');
-});
